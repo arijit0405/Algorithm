376fc0cbd0824b561c0abe7447975231efd69ec5
arijit0405 <arijitchakraborty691@gmail.com>
2022-10-14 00:45:28 +0530

    finding integer

--- src/com/arijit/array/FirstProg.java
+++ src/com/arijit/array/FirstProg.java
@@ -2,6 +2,20 @@ package com.arijit.array;
 
 public class FirstProg {
     public static void main(String[] args) {
-        System.out.println("change!");
+        int[] marks= {2,3,4,5,6};
+        boolean match= false;
+        int l= marks.length;
+        for(int i=0; i<l; i++){
+            if (marks[i]==7){
+                match=true;
+                break;
+            }
+        }if(match==true){
+            System.out.println("found");
+        }else {
+            System.out.println("not foun");
+
+        }
+
     }
 }

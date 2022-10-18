032a4f8abcda6e0a657df853f326e73df98c128b
arijit0405 <arijitchakraborty691@gmail.com>
2022-10-18 23:45:33 +0530

    reverse

---
+++ src/com/arijit/array/reverse.java
@@ -0,0 +1,19 @@
+package com.arijit.array;
+
+public class reverse {
+    public static void main(String[] args) {
+        int[] arr = {1, 21, 3, 4, 5, 34, 67};
+        int l = arr.length;
+        int n = Math.floorDiv(l, 2);
+        int temp;
+
+        for (int i = 0; i < n; i++) {
+            temp = arr[i];
+            arr[i] = arr[l - i - 1];
+            arr[l - i - 1] = temp;
+        }
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+    }
+}

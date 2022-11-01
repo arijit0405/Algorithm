5d1dc0b351e9f2cfa805af3f174192beaddfe11b
arijit0405 <arijitchakraborty691@gmail.com>
2022-11-02 00:05:16 +0530

    exception handling 2

---
+++ src/com/arijit/exceptionHandling/TryCatch2.java
@@ -0,0 +1,19 @@
+package com.arijit.exceptionHandling;
+class TryCatch2 {
+
+    public static void main(String[] args) {
+        int i=50;
+        int j=0;
+        int data;
+        try
+        {
+            data=i/j;
+        }
+
+        catch(Exception e)
+        {
+
+            System.out.println(i/(j+2));
+        }
+    }
+}
